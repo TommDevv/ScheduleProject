2259b9fd42c2659da7229b25c0b6a86a53f16c54
TommDevv <tomasonic2@gmail.com>
2026-05-16 15:57:36 -0500

    updated cors policy

--- backend-django/config/settings.py
+++ backend-django/config/settings.py
@@ -67,6 +67,11 @@ CORS_ALLOWED_ORIGINS=[
     "http://127.0.0.1:3000",
 ]
 
+##Permitir todos los origenes (no recomendado para producción)
+## Configurado para pruebas, desactivar y configurar manualmente en producción real.
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_HEADERS = list(default_headers) + [
     "authorization",
     "Authorization"
--- frontend-React/front-react/src/enviroment.js
+++ frontend-React/front-react/src/enviroment.js
@@ -1,5 +1,9 @@
 export const enviroment = {
     production: false,
 
-    urlApi: 'http://127.0.0.1:8000/'
+    //Variable para entorno local
+    // urlApi: 'http://127.0.0.1:8000/'
+
+    //Variable para despliegue en render
+    urlApi: 'https://scheduleproject-fol7.onrender.com/'
 };
